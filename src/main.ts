acbc039b5f2d512aaa4ca3e256d5669a4d089b7a
lssibb <lssibb@yandex.ru>
2026-04-08 02:13:20 +0700

    edit mode added

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
 import { Add, Boil, CoffeBean, Drink, Grind, Ice, Milk, Mix, Pour, Syrup, Water, Whip } from "./models";
 import { Store } from "./store";
+import './style.css';
 import type { Element } from "./models";
 
 
 const store = new Store();
 
 const creationInput = document.getElementById("drink-name")!;
-const btnCreate = document.getElementById("btn-create")!;
+const btnCreateOrSave = document.getElementById("btn-create")!;
 const elementSelection = document.getElementById("element-select")!;
 const btnAdd = document.getElementById("btn-add-element")!;
 const weightInput = document.getElementById("weight-input")!;
 const currentDrinkDescription = document.getElementById("current-drink")!;
 let currentElements: Element[] = [];
+let editingId: string | null = null;
 
 btnAdd.addEventListener("click", () => {
   let element : Element;
@@ -36,10 +38,20 @@ btnAdd.addEventListener("click", () => {
 });
 
 
-btnCreate.addEventListener("click", () => {
-  const name = (creationInput as HTMLInputElement).value;
-  const drink = new Drink(name, [...currentElements]);
-  store.addDrink(drink);
+btnCreateOrSave.addEventListener("click", () => {
+  if(editingId!=null){
+    const drink = store.getDrink(editingId);
+    if (!drink) return;
+    drink.name = (creationInput as HTMLInputElement).value;
+    drink.elements = [...currentElements];
+    editingId = null;
+  }
+  else{
+    const name = (creationInput as HTMLInputElement).value;
+    const drink = new Drink(name, [...currentElements]);
+    store.addDrink(drink);
+  }
+  btnCreateOrSave.textContent="Создать";
   currentElements.length = 0;
   renderСurrentElements();
   renderDrinks();
@@ -55,15 +67,30 @@ const drinksList = document.getElementById("drinks-list")!;
 function renderDrinks(){
   drinksList.innerHTML = "";
   for(const drink of store.getAllDrinks()){
-    const btnDeleteElement = document.createElement('button');
+    const btnDeleteDrink = document.createElement('button');
+    const btnEditDrink = document.createElement('button');
     const line = document.createElement('div');
+
     line.textContent = `${drink.name}: ${drink.elements.map(e => e.describe()).join(", ")}`;
-    btnDeleteElement.textContent='❌';
-    btnDeleteElement.addEventListener("click", () => {
+
+    btnEditDrink.textContent='🖋';
+    btnEditDrink.addEventListener("click", () => {
+      (creationInput as HTMLInputElement).value = drink.name;
+      currentElements = [...drink.elements];
+      editingId = drink.id;
+      btnCreateOrSave.textContent="Редактировать";
+      renderСurrentElements();
+      renderDrinks();
+    })
+    line.appendChild(btnEditDrink);
+
+    btnDeleteDrink.textContent='❌';
+    btnDeleteDrink.addEventListener("click", () => {
       store.deleteDrink(drink.id);
       renderDrinks();
     })
-    line.appendChild(btnDeleteElement);
+    line.appendChild(btnDeleteDrink);
+
     drinksList.appendChild(line);
   }
 }
